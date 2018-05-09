4da47bee0ed26244c3add66fcb433099e456edc0
Sir Rufo <oliver.muenzberg@gmail.com>
2018-05-09 09:08:00 +0200

    MultiTargetLibrary

---
+++ 50245414/MultiTargetLibrary/ConsoleApp.NetFull/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleApp.NetFull
+{
+    class Program
+    {
+        static void Main( string[] args )
+        {
+            Console.WriteLine( MyClassLibrary.Foo.Info );
+        }
+    }
+}
---
+++ 50245414/MultiTargetLibrary/MyClassLibrary/Foo.cs
@@ -0,0 +1,23 @@
+﻿namespace MyClassLibrary
+{
+    public class Foo
+    {
+        public static string Info { get; } = "Conditionals"
+#if net461
+            + " net461"
+#endif
+#if NET461
+            + " NET461"
+#endif
+#if NETCORE
+            + " NETCORE"
+#endif
+#if NETSTANDARD
+            + " NETSTANDARD"
+#endif
+#if NETSTANDARD2_0
+            + " NETSTANDARD2_0"
+#endif
+            + "";
+    }
+}
