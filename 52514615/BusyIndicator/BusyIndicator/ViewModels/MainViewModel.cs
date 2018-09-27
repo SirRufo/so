237a540072362be17e5986cfb3f700830148686c
Sir Rufo <oliver.muenzberg@gmail.com>
2018-09-27 07:54:14 +0200

    BusyIndicator on LoadData

--- 52514615/BusyIndicator/BusyIndicator/ViewModels/MainViewModel.cs
+++ 52514615/BusyIndicator/BusyIndicator/ViewModels/MainViewModel.cs
@@ -1,6 +1,29 @@
-﻿namespace BusyIndicator.ViewModels
+﻿using ReactiveUI;
+
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace BusyIndicator.ViewModels
 {
     public class MainViewModel : Base.ViewModelBase
     {
+        public ICommand ReloadDataCommand => ReactiveCommand.CreateFromTask(ReloadDataAsync);
+
+        private Task ReloadDataAsync()
+        {
+            return LoadDataAsync();
+        }
+
+        private async Task LoadDataAsync()
+        {
+            IsBusy = true;
+            await Task.Delay(1500);
+            IsBusy = false;
+        }
+
+        public override Task InitializeAsync(object parameter)
+        {
+            return LoadDataAsync();
+        }
     }
 }
