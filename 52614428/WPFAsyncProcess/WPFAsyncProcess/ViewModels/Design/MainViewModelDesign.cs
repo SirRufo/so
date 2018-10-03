08128e97c49dae6cea99a541580afb6088d7ec25
Sir Rufo <oliver.muenzberg@gmail.com>
2018-10-03 19:56:55 +0200

    MainViewModelDesign added

---
+++ 52614428/WPFAsyncProcess/WPFAsyncProcess/ViewModels/Design/MainViewModelDesign.cs
@@ -0,0 +1,14 @@
+﻿using System.Linq;
+
+using WPFAsyncProcess.Services.Fake;
+
+namespace WPFAsyncProcess.ViewModels.Design
+{
+    public class MainViewModelDesign : MainViewModel
+    {
+        public MainViewModelDesign() : base(new FakeFileProcessor())
+        {
+            LogLines = Enumerable.Range(1, 10).Select(e => new Models.LogLine { Value = e.ToString(), }).ToList();
+        }
+    }
+}
