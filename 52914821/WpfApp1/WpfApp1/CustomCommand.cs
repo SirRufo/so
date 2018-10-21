65e78e9ff32eaaccb20ecfad0d41ac67e03ae8ec
Sir Rufo <oliver.muenzberg@gmail.com>
2018-10-21 13:58:47 +0200

    commit project

---
+++ 52914821/WpfApp1/WpfApp1/CustomCommand.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows.Input;
+
+namespace WpfApp1
+{
+    public static class CustomCommand
+    {
+        public static RoutedUICommand Save { get; } = new RoutedUICommand(
+            text: "Save",
+            name: "Save",
+            ownerType: typeof(CustomCommand),
+            inputGestures: new InputGestureCollection()
+            {
+                new KeyGesture( Key.F2 ),
+            });
+    }
+}
