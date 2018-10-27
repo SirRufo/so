ddcb112233d8680712dbe4761f83c5a53903048c
Sir Rufo <oliver.muenzberg@gmail.com>
2018-10-27 08:22:02 +0200

    commit

---
+++ 53016687/WpfApp1/WpfApp1/ViewModels/Base/ViewModelBase.cs
@@ -0,0 +1,21 @@
+﻿using System.Threading.Tasks;
+
+using WpfApp1.Utils;
+
+namespace WpfApp1.ViewModels.Base
+{
+    public abstract class ViewModelBase : BindableObject
+    {
+        private bool _isBusy;
+        public bool IsBusy
+        {
+            get => _isBusy;
+            protected set => Set(ref _isBusy, value);
+        }
+
+        public virtual Task InitializeAsync(object parameter)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ 53016687/WpfApp1/WpfApp1/ViewModels/MainViewModel.cs
@@ -0,0 +1,31 @@
+﻿namespace WpfApp1.ViewModels
+{
+    public class MainViewModel : Base.ViewModelBase
+    {
+        private bool _quickProcess;
+        public bool QuickProcess
+        {
+            get => _quickProcess;
+            set
+            {
+                if (Set(ref _quickProcess, value))
+                {
+                    RaisePropertyChanged(nameof(UploadToYoutube));
+                    RaisePropertyChanged(nameof(UploadToYoutubeEnabled));
+                }
+            }
+        }
+
+        private bool _uploadToYoutube;
+        public bool UploadToYoutube
+        {
+            get => QuickProcess ? false : _uploadToYoutube;
+            set => Set(ref _uploadToYoutube, value);
+        }
+
+        public bool UploadToYoutubeEnabled
+        {
+            get => !QuickProcess;
+        }
+    }
+}
